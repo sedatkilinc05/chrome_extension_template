3e60f7ca14e282828e55edf2e722dce8d466472e
sedatkilinc05 <sedatkilinc05@gmail.com>
2025-10-13 05:58:49 +0300

    feat: add initial manifest and icon files for Chrome extension to public folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        side_panel: 'index.html'
+      },
+      output: {
+        entryFileNames: '[name].js'
+      },
+    },
+    outDir: 'dist',
+  },
   plugins: [react()],
 })
